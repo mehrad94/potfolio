182e4528ce4da334408c4111ead790c1f74845e8
MehradFarahnak <mehradfarahnak9@gmail.com>
2023-09-02 19:09:32 +0330

    change png to webp

--- app/components/TopLeftImg.tsx
+++ app/components/TopLeftImg.tsx
@@ -6,7 +6,7 @@ type Props = {};
 function TopLeftImg({}: Props) {
   return (
     <div className='absolute left-0 top-0 mix-blend-color-dodge z-0 w-[200] xl:w-[400] opacity-50'>
-      <Image src='/top-left-img.png' width={400} height={400} alt='' />
+      <Image src='/top-left-img.webp' width={400} height={400} alt='' />
     </div>
   );
 }
